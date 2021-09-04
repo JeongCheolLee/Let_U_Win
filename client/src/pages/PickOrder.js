d8cd9a6661d1fe657d0df88c3acca0b2a12bf8f0
JeongCheolLee <85550653+JeongCheolLee@users.noreply.github.com>
2021-09-04 16:27:43 +0900

    jc

---
+++ client/src/pages/PickOrder.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
+function PickOrder() {
+    return (
+      <BrowserRouter>
+            <h1>Let U Win</h1>
+      </BrowserRouter>
+
+    )
+}
+
+export default PickOrder
