a7110e0e43701092498390ccddad0cf2f8924119
JeongCheolLee <85550653+JeongCheolLee@users.noreply.github.com>
2021-09-11 21:27:00 +0900

    here u are

--- client/src/components/image_list.js
+++ client/src/components/image_list.js
@@ -4,38 +4,38 @@ import ImageList from '@material-ui/core/ImageList';
 import ImageListItem from '@material-ui/core/ImageListItem';
 import ImageListItemBar from '@material-ui/core/ImageListItemBar';
 
+
 const itemData = [
   {
-    img: 'https://github.com/JeongCheolLee/Let_U_Win/blob/main/client/test(temp)/aatrox.jpg?raw=true',
+    img: '/images/champions/aatrox.jpg',
     title: '아트록스',
     winrate: '55%'
   }, 
   {
-    img: 'https://github.com/JeongCheolLee/Let_U_Win/blob/main/client/test(temp)/ahri.jpg?raw=true',
+    img: '/images/champions/ahri.jpg',
     title: '아리',
     winrate: '55%'
   },
   {
-    img: 'https://github.com/JeongCheolLee/Let_U_Win/blob/main/client/test(temp)/akali.jpg?raw=true',
+    img: '/images/champions/akali.jpg',
     title: '아칼리',
     winrate: '55%'
   },
   {
-    img: 'https://github.com/JeongCheolLee/Let_U_Win/blob/main/client/test(temp)/akshan.jpg?raw=true',
+    img: '/images/champions/akshan.jpg',
     title: '아크샨',
     winrate: '55%'
   },
   {
-    img: 'https://github.com/JeongCheolLee/Let_U_Win/blob/main/client/test(temp)/anivia.jpg?raw=true',
+    img: '/images/champions/anivia.jpg',
     title: '애니비아',
     winrate: '55%'
   },
   {
-    img: 'https://github.com/JeongCheolLee/Let_U_Win/blob/main/client/test(temp)/Aphelios.jpg?raw=true',
+    img: '/images/champions/Aphelios.jpg',
     title: '아펠리오스',
     winrate: '55%'
   },
-
 ];
 
 
@@ -46,7 +46,6 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'space-around',
     overflow: 'hidden',
   //  backgroundColor: theme.palette.background.paper,
-    
   },
   imageList: {
     width: 700,
@@ -57,28 +56,52 @@ const useStyles = makeStyles((theme) => ({
     fontFamily: '궁서',
     fontWeight: 'bold'
   }
-})); 
+}));
+
+
 
 export default function TitlebarImageList() {
   const classes = useStyles();
+  
+
+  const defaultMessage = (
+    <div >
+      <br />
+      <br />
+      <br />
+      기본메시지입니다.
+    </div>
+  )
+
+  const choice = (arg) =>{
+
+    return(
+      <div style={{fontFamily: '궁서', fontWeight: 'bold', fontSize: '25px'}}>
+        역시, {arg}는 탁월한 선택입니다.
+      </div>
+    );
+  }
+
 
   return (
     <div className={classes.root}>
       <ImageList rowHeight={180} className={classes.imageList} cols={6}>
         {itemData.map((item) => (
           <ImageListItem key={item.img}>
-            <img src={item.img} alt={item.title}/>
+            <img onClick = { () => choice(item.title)} src={item.img} alt={item.title} style={{cursor: "pointer"}}/>
             <ImageListItemBar 
               title={item.title}
               subtitle={item.winrate}
               classes={{title:classes.title}}
               style={{ height: '40px'}
-              }  
+              }
             />
           </ImageListItem>
         ))}
       </ImageList>
+      {defaultMessage}
     </div>
+
   );
 }
 
--- client/src/pages/FirstPick.js
+++ client/src/pages/FirstPick.js
@@ -1,35 +1,26 @@
 import React from 'react';
 import TitlebarImageList from '../components/image_list'
-import background from '../images/background/hanji2.jpg'
+
 
 function FirstPick({location}) {
     const lane = location.state;
 
     const goongseo = {fontWeight:"bold", fontFamily:"궁서"}
 
-    const backgroundset = {
-        backgroundImage: `url(${background})`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        width: '100vw',
-        height: '100vh'
-    }
-
     return (
-        <div align = "center" style = {{backgroundset}}>
+        <div align = "center">
             <h1 style={goongseo}>
                 선픽이시군요! <br />
                 요즘 {lane}포지션에서 승률 좋은 챔피언을 추천드릴게요!<br /><br /><br />
             </h1>
 
-            <p style = {{...goongseo, fontSize:'25px'}}>
+            <div style = {{...goongseo, fontSize:'25px'}}>
                 플레이 하고 싶은 챔피언을 선택해주세요
-            </p>
+            </div>
 
-            <p>
+            <div>
             {TitlebarImageList()}
-            </p>
+            </div>
         </div>
     )
 }
