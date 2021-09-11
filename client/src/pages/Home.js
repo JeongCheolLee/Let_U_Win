498c6f4237ac0dfe44b6132c48d55d5834bf6df1
lhjeong60 <lhjeong60@naver.com>
2021-09-11 23:24:01 +0900

    test1

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,9 @@
-import {React, useState} from 'react';
-import {Button, Container} from '@material-ui/core';
+import {React, useState, useRef} from 'react';
+import {Button} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 function Home({history}) {
-    const [lane, setLane] = useState("none");
+    const [lane, setLane] = useState('none');
 
     const changeHandler = (e) => {
         setLane(e.target.value);
@@ -11,65 +11,67 @@ function Home({history}) {
 
     const btnClickHandler = () => {
         history.push({
-            pathname:"/pickorder",
+            pathname:'/pickorder',
             state:lane
         });
     }
 
     const useStyles = makeStyles((theme) => ({
         laneBtn:{
-            width:"100px",
-            height:"100px",
-            margin:"10px",
-            // backgroundColor:"black",
-            padding:"0px",
-            fontSize:"35px",
-            fontWeight:"bold",
-            fontFamily:"궁서",
+            width:'100px',
+            height:'100px',
+            margin:'10px',
+            // backgroundColor:'black',
+            padding:'0px',
+            fontSize:'35px',
+            fontWeight:'bold',
+            fontFamily:['궁서', '궁서체'],
         }
     }))
 
     const classes = useStyles();
 
+    const btnRef = useRef();
+
     const lanes = [
         {
-            ko:"탑",
-            en:"top",
+            ko:'탑',
+            en:'top',
         },
         {
-            ko:"정글",
-            en:"jungle",
+            ko:'정글',
+            en:'jungle',
         },
         {
-            ko:"미드",
-            en:"middle",
+            ko:'미드',
+            en:'middle',
         },
         {
-            ko:"원딜",
-            en:"bottom",
+            ko:'원딜',
+            en:'bottom',
         },
         {
-            ko:"서폿",
-            en:"support",
+            ko:'서폿',
+            en:'support',
         },
     ]
 
     return (
         <div align='center'>
             {/* <div>
-                <select name="lane" onChange={changeHandler}>
-                    <option value="none" selected>which?</option>
-                    <option value="top">top</option>
-                    <option value="jungle">jungle</option>
-                    <option value="mid">mid</option>
-                    <option value="bottom">bottom</option>
-                    <option value="supporter">supporter</option>
+                <select name='lane' onChange={changeHandler}>
+                    <option value='none' selected>which?</option>
+                    <option value='top'>top</option>
+                    <option value='jungle'>jungle</option>
+                    <option value='mid'>mid</option>
+                    <option value='bottom'>bottom</option>
+                    <option value='supporter'>supporter</option>
                 </select>
                 <button onClick={btnClickHandler}>OK</button>
             </div> */}
             {lanes.map((item) => {
                 return (
-                    <Button variant="contained" size="large" className={classes.laneBtn}>
+                    <Button onClick={} variant='contained' size='large' className={classes.laneBtn}>
                         {item.ko}
                     </Button>    
                 )
