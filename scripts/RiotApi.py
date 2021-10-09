b0e720a68953a5e56a57a7ef5e9df36a9d50f1a7
JeongCheolLee <85550653+JeongCheolLee@users.noreply.github.com>
2021-10-09 19:58:27 +0900

    Update RiotApi.py
    script for linux

--- scripts/RiotApi.py
+++ scripts/RiotApi.py
@@ -1,7 +1,8 @@
 import requests
 from urllib.parse import urlparse
+import time
 
-
+api_key = 'RGAPI-5274f047-ce83-4884-aed4-61d158c09582'
 
 def getChallengerUser():
     url = 'https://kr.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key='+api_key
@@ -40,20 +41,32 @@ def getMasterUser():
     return user
 
 
-https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/hide%20on%20bush?api_key=RGAPI-5274f047-ce83-4884-aed4-61d158c09582
-
 def getPuuid(id_list):
     puuid_list = []
+    err_list = []
     
+    cnt = 0
+    length = str(len(id_list))
     for sum_id in id_list:
         try:
+            cnt += 1
             url = 'https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+ sum_id +'?api_key='+api_key
-            result = requests.get(urlparse(url).geturl()).json()
-            puuid = result['puuid']
+            result = requests.get(urlparse(url).geturl())
+            
+            while result.status_code == 429: ## wait untill rate limit ended
+                result = requests.get(urlparse(url).geturl())
+                time.sleep(5)
+                print("waiting.. because of rate limit")
+                
+            puuid = result.json()['puuid']
             puuid_list.append(puuid)
+            print(str(cnt) + " /" + length + " finished")
             
         except:
+            time.sleep(30)
+            print("waiting for 30sec")
             pass
+        
         
     return puuid_list
 
@@ -61,10 +74,28 @@ def getPuuid(id_list):
 def getMatchId(puuid_list):
     match_id_list = []
     
+    cnt = 0
+    length = str(len(puuid_list))
     for puuid in puuid_list:
-        url = 'https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/'+ puuid +'/ids?type=ranked&start=0&count=100&api_key='+ api_key
-        result = requests.get(urlparse(url).geturl()).json()
-        match_id_list.append(result)
+        try:
+            cnt += 1            
+            url = 'https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/'+ puuid +'/ids?type=ranked&start=0&count=100&api_key='+ api_key
+            result = requests.get(urlparse(url).geturl())
+            
+            while result.status_code == 429: ## wait untill rate limit ended
+                result = requests.get(urlparse(url).geturl())
+                time.sleep(5)
+                print("waiting.. because of rate limit")
+                
+            match_id_list.append(result.json())
+                
+            print(str(cnt) + " /" + length + " finished")
+                
+        except:
+            print("waiting for 30sec")
+            time.sleep(30)
+            print("wrong with " + puuid)
+            pass
         
     match_id_list = sum(match_id_list,[])
     match_id_list = list(set(match_id_list))
@@ -76,27 +107,77 @@ from pymongo import MongoClient
 
 def getMatchData(match_id_list):
 
-    mongodb_URI = "mongodb+srv://<id>:<pw>@master.kxfv4.mongodb.net/test?retryWrites=true&w=majority"
+    mongodb_URI = "mongodb://localhost:27017/"
     client = MongoClient(mongodb_URI)
     
     db = client['main']
     collection = db['match']
     
-    collection.insert_one(result)
+    cnt = 0
+    length = str(len(match_id_list))
     
     for match_id in match_id_list:
-        url = 'https://asia.api.riotgames.com/lol/match/v5/matches/'+ match_id + '?api_key=' + api_key
-        result = requests.get(urlparse(url).geturl()).json()
+        cnt += 1
         
+        try:
+            url = 'https://asia.api.riotgames.com/lol/match/v5/matches/'+ match_id + '?api_key=' + api_key
+            result = requests.get(urlparse(url).geturl())
+            
+            while result.status_code == 429: ## wait untill rate limit ended
+                result = requests.get(urlparse(url).geturl())
+                time.sleep(5)
+                print("waiting.. because of rate limit")
+                
+            collection.insert_one(result.json())
+            print(str(cnt) + " /" + length + " added")
+        except:
+            time.sleep(30)
+            print("waiting for 30sec")
+            pass
+        
+        
+    return print("finished!")     
+
+
+
+def getMatchTimeLineData(match_id_list):
+
+    mongodb_URI = "mongodb://localhost:27017/"
+    client = MongoClient(mongodb_URI)
     
-        
-
-
-        
+    db = client['main']
+    collection = db['timeline']
     
-
-getChallengerUser()
-getGrandmasterUser()
-getMasterUser()
-
+    cnt = 0
+    length = str(len(match_id_list))
+    
+    for match_id in match_id_list:
+        cnt += 1
+        try:
+            url = 'https://asia.api.riotgames.com/lol/match/v5/matches/'+ match_id + '/timeline?api_key=' + api_key
+            result = requests.get(urlparse(url).geturl())
+            
+            while result.status_code == 429: ## wait untill rate limit ended
+                result = requests.get(urlparse(url).geturl())
+                time.sleep(5)
+                print("waiting.. because of rate limit")
+                
+            collection.insert_one(result.json())
+            print(str(cnt) + " /" + length + " added")
+        except:
+            time.sleep(30)
+            print("waiting for 30sec")
+            pass
         
+        
+    return print("finished!")    
+
+
+
+chal = getChallengerUser()
+puuid = getPuuid(chal)
+matchId = getMatchId(puuid)
+getMatchData(matchId)
+getMatchTimeLineData(matchId)
+
+
