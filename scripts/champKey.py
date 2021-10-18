aeb83d3b14ac8e0385862ec5807557254e3b6abf
JeongCheolLee <85550653+JeongCheolLee@users.noreply.github.com>
2021-10-18 14:24:29 +0900

    getChampionsKey
    key : champKey
    value : championName

---
+++ scripts/champKey.py
@@ -0,0 +1,29 @@
+from pymongo import MongoClient
+
+mongodb_URI = "mongodb://localhost:27017/"
+client = MongoClient(mongodb_URI)
+
+#client = MongoClient("3.35.222.47",27017)
+#db = client['main']
+
+
+#데이터베이스 확인
+#print(client.list_database_names())
+db = client['main']
+#collection = db['match']
+
+
+#챔피언 이름 - 키 매칭
+collection = db['champions']
+result = collection.find()
+
+champKey = {}
+
+for champ in result:
+    champKey[champ['key']] = champ['id']
+
+champKey = dict(sorted(champKey.items()))
+
+
+collection = db['championKeys']
+collection.insert_one(champKey)
