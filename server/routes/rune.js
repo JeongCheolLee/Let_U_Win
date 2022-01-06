1178353419e99e257e3a87a276cbb002787f20fa
lhjeong60 <lhjeong60@naver.com>
2022-01-06 20:57:34 +0900

    dummy commit

--- server/routes/rune.js
+++ server/routes/rune.js
@@ -32,19 +32,40 @@ router.get('/:lane/:mypick/:enemypick', (req, res) => {
     const myPick = FirstCharUpperCase(req.params.mypick);
     const enemyPick = FirstCharUpperCase(req.params.enemypick);
 
-    // const findObjectLiteral = {};
+    // PerksModel.find({ [myPick]: { $exists: true } }).exec((err, docs) => {
+    //     if (err) return res.json({ success: false, err });
+    //     console.log(docs.length);
+    //     // console.log(docs);
+    //     return res.status(200).json({ success: true });
+    // });
 
-    // findObjectLiteral[[myPick]] = { $exists: true };
+    // PerksModel.where('Aatrox')
+    //     .exists()
+    //     .exec((err, docs) => {
+    //         if (err) return res.json({ success: false, err });
 
-    // const findObjectLiteral = { [myPick]: { $exists: true } };
-    // console.log(findObjectLiteral);
+    //         if (docs.length === 1) {
+    //             console.log(docs);
+    //         } else {
+    //             console.log(docs.length);
+    //             // console.log(docs);
+    //         }
+    //         return res.status(200).json({ success: true });
+    //     });
 
-    PerksModel.find({ [myPick]: { $exists: true } }).exec((err, docs) => {
-        if (err) return res.json({ success: false, err });
-        console.log(docs.length);
-        // console.log(docs);
-        return res.status(200).json({ success: true });
-    });
+    PerksModel.find()
+        .exists('akali', true)
+        .exec((err, docs) => {
+            if (err) return res.json({ success: false, err });
+
+            if (docs.length === 1) {
+                console.log(docs);
+            } else {
+                console.log(docs.length);
+                // console.log(docs);
+            }
+            return res.status(200).json({ success: true });
+        });
 
     // res.status(200).send('success');
 });
