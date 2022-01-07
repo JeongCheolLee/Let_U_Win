13360e0afdb7b987417db7cfea8cffdd90259a5b
JeongCheolLee <85550653+JeongCheolLee@users.noreply.github.com>
2022-01-07 18:05:55 +0900

    temp logo added

--- client/src/components/ImageListHjlee.js
+++ client/src/components/ImageListHjlee.js
@@ -5,57 +5,57 @@ import ImageListItem from '@material-ui/core/ImageListItem';
 import ImageListItemBar from '@material-ui/core/ImageListItemBar';
 
 const useStyles = makeStyles((theme) => ({
-  imageList: {
-    width: 560,
-    height: 550,
-    alignContent: 'flex-start',
-  },
-  title: {
-    fontSize: '0.6em',
-    fontFamily: ['궁서', '궁서체'],
-    fontWeight: 'bold',
-  },
-  titleWrap: {
-    margin: '8px',
-  },
+    imageList: {
+        width: 560,
+        height: 550,
+        alignContent: 'flex-start',
+    },
+    title: {
+        fontSize: '0.6em',
+        fontFamily: ['궁서', '궁서체'],
+        fontWeight: 'bold',
+    },
+    titleWrap: {
+        margin: '8px',
+    },
 }));
 
 export default function ImageListHjlee(props) {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  const url = useRef(
-    'http://ddragon.leagueoflegends.com/cdn/11.19.1/img/champion/'
-  );
-  const itemData = props.filteredChampionsList;
-  const imgOnClickHandler = (e) => {
-    props.getPickFromImageList(e.target.alt);
-  };
+    const itemData = props.filteredChampionsList;
+    const imgOnClickHandler = (e) => {
+        props.getPickFromImageList(e.target.alt);
+    };
 
-  return (
-    <div>
-      <ImageList rowHeight="auto" className={classes.imageList} cols={6}>
-        {itemData.map((item) => (
-          <ImageListItem
-            style={{ height: '90px', width: '90px' }}
-            key={item.id}
-          >
-            <img
-              onClick={imgOnClickHandler}
-              src={
-                'http://ddragon.leagueoflegends.com/cdn/11.19.1/img/champion/' +
-                item.image.full
-              }
-              alt={item.name}
-              style={{ cursor: 'pointer', width: '90px' }}
-            />
-            <ImageListItemBar
-              title={item.name}
-              classes={{ title: classes.title, titleWrap: classes.titleWrap }}
-              style={{ height: '30px', textAlign: 'left' }}
-            />
-          </ImageListItem>
-        ))}
-      </ImageList>
-    </div>
-  );
+    return (
+        <div>
+            <ImageList rowHeight="auto" className={classes.imageList} cols={6}>
+                {itemData.map((item) => (
+                    <ImageListItem
+                        style={{ height: '90px', width: '90px' }}
+                        key={item.id}
+                    >
+                        <img
+                            onClick={imgOnClickHandler}
+                            src={
+                                'http://ddragon.leagueoflegends.com/cdn/11.19.1/img/champion/' +
+                                item.image.full
+                            }
+                            alt={item.name}
+                            style={{ cursor: 'pointer', width: '90px' }}
+                        />
+                        <ImageListItemBar
+                            title={item.name}
+                            classes={{
+                                title: classes.title,
+                                titleWrap: classes.titleWrap,
+                            }}
+                            style={{ height: '30px', textAlign: 'left' }}
+                        />
+                    </ImageListItem>
+                ))}
+            </ImageList>
+        </div>
+    );
 }
